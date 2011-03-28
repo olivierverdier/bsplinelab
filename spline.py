08ca375842973e97c44dda03d072ae4fff9fd35a
Olivier Verdier <olivier.verdier@gmail.com>
2011-03-28 12:39:04 +0200

    remove trailing spaces

--- spline.py
+++ spline.py
@@ -8,15 +8,15 @@ from IPython.Debugger import Tracer
 dh = Tracer()
 
 class BSpline(object):
-	
+
 	def __init__(self, points, knots):
 		self.points = points
 		self.knots = knots
 		self.length = len(knots) - len(points)
 		self.degree = self.length + 1
-	
+
 	ktol = 1e-1
-	
+
 	def find(self, t):
 		"""
 		Unfinished function to find out between which node a time t is.
@@ -36,18 +36,18 @@ class BSpline(object):
 ## 		# otherwise take the left node
 ## 		candidate = (dist > 0).argmax() - 1
 ## 		return candidate
-	
+
 	def plot_points(self):
 		"""
 		Plot the control points.
 		"""
 		plot(self.points[:,0],self.points[:,1],'ro:')
-	
+
 ## 	def plot_knots(self):
 ## 		kns = self.knots[self.length:-self.length]
 ## 		pts = array([self(kn,i) for i,kn in enumerate(kns[:-1])])
 ## 		plot(pts[:,0],pts[:,1],'sg')
-	
+
 	plotres = 200
 
 	def generate_points(self, k_range=None):
@@ -60,8 +60,8 @@ class BSpline(object):
 			left, right = self.knots[k], self.knots[k+1]
 			times = linspace(left, right, self.plotres * (right-left) + 1)
 			yield (times,k,self(times,k))
-	
-	
+
+
 	def plot(self, knot=None, with_knots=False):
 		"""
 		Plot the curve.
@@ -71,7 +71,7 @@ class BSpline(object):
 			plot(val[:,0],val[:,1], label="%1.f - %1.f" % (self.knots[k], self.knots[k+1]))
 			if with_knots:
 				plot(val[[0,-1],0], val[[0,-1],1], 'gs')
-	
+
 	def plot_basis(self):
 		"""
 		Plot the basis function for the given knot (or all of them)
@@ -79,8 +79,8 @@ class BSpline(object):
 		save_pts = self.points
 		for k in k_range:
 			self.points = zeros(len(self.knots))
-			
-		
+
+
 	def __call__(self, t, lknot=None):
 		if lknot is None:
 			if isscalar(t):
@@ -99,7 +99,7 @@ class BSpline(object):
 		# we put the time on the first index; all other arrays must be reshaped accordingly
 		t = t.reshape(-1,1,1)
 		pts = pts[newaxis,...]
-		
+
 		for n in reversed(1+arange(self.degree)):
 			diffs = kns[n:] - kns[:-n]
 			lcoeff = (kns[n:] - t)/diffs
@@ -110,13 +110,13 @@ class BSpline(object):
 		if scalar_t:
 			result = squeeze(result) # test this
 		return result
-	
+
 ## class BSpline_basis(BSpline):
 ## 	def __init__(self, *args, **kwargs):
 ## 		super(BSpline_basis, self).__init__(self, *args, **kwargs)
 ## 		self.points = arange(len(self.points)).reshape(-1,1)
-	
-			
+
+
 
 if __name__ == '__main__':
 	ex1 = {
@@ -128,19 +128,19 @@ if __name__ == '__main__':
 	'pts': array([[1.,2], [2,3], [2,5], [1,6], [1,9]]),
 	'knots': array([1.,2.,3.,4.,5.,6.,7.])
 	}
-	
+
 	# only C0
 	ex3 = {
 	'pts': array([[1.,2], [2,3], [2,5], [1,6], [1,9], [2,11], [2,9]]),
 	'knots': array([1.,2.,3.,4.,4.,4.,5.,6.,6.])
 	}
-	
+
 	# discontinuous
 	ex4 = {
 	'pts': array([[1.,2], [2,3], [2,5], [1,6], [1,9], [2,11], [2,9], [1.5,8]]),
 	'knots': array([1.,2.,3.,4.,4.,4.,4.,5.,6.,6.])
 	}
-	
+
         deBoor=[[0.7,-0.4],[1.0,-0.4],[2.5,-1.2],[3.2,-.5],[-0.2,-.5],[.5,-1.2],[2.0,-.4],[2.3,-.4]]
         param=[1.,1.,1.,1.2,1.4,1.6,1.8,2.,2.,2.]
 
@@ -149,16 +149,16 @@ if __name__ == '__main__':
 	ex_Claus['knots'] = array(param)
 
 	ex = ex4
-	
+
 	s = BSpline(ex['pts'], ex['knots'])
-	
+
 	print s(array([3.2,3.5]), 2)
 	from pylab import clf
 	clf()
 #	s.plot_points()
 #	s.plot_knots()
 	s.plot(with_knots=True)
-	
+
 ## 	deBoor = array([[0.7,-0.4],
 ## 				[1.0,-0.4],
 ## 				[2.5,-1.2],
