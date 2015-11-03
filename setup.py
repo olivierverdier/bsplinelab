c96a2225b75a5816312f861a822a4f8a5290b1ef
Olivier Verdier <olivier.verdier@gmail.com>
2015-11-03 18:59:11 +0100

    add travis and setup files

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: UTF-8
+
+
+from distutils.core import setup
+
+setup(
+	name         = 'spline',
+	version      = '0.1',
+	description  = 'B-Splines',
+	author = 'Olivier Verdier',
+	url = 'https://github.com/olivierverdier/bsplinelab',
+	license      = 'MIT',
+	keywords = ['Math', 'Homogeneous space',],
+	packages=['homogint',],
+	classifiers = [
+	'Development Status :: 4 - Beta',
+	'Intended Audience :: Science/Research',
+	'License :: OSI Approved :: MIT License',
+	'Operating System :: OS Independent',
+	'Programming Language :: Python',
+	'Topic :: Scientific/Engineering :: Mathematics',
+	],
+	)
