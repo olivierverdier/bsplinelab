aa9db4eeecad03498e17492a5fefd755080759ab
Olivier Verdier <olivier.verdier@gmail.com>
2016-01-12 10:32:55 +0100

    spaces instead of tabs

--- spline.py
+++ spline.py
@@ -6,24 +6,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class Knots(object):
-	"""
+    """
 Knots class.
 
 If:
-	P = Nb points
-	C = Nb curves
-	K = Nb knots
-	D = Degree
+    P = Nb points
+    C = Nb curves
+    K = Nb knots
+    D = Degree
 
 then the following relations are implemented in the code:
 
-	C = K - 2D + 1
-	D = K - P + 1
+    C = K - 2D + 1
+    D = K - P + 1
 
 This also gives
 
-	C + D = P
-	K + 1 = P + D
+    C + D = P
+    K + 1 = P + D
 
 For example, suppose that there are P=n+1 control points:
 
@@ -36,177 +36,177 @@ n+2      n-1       2
 ...      ...       ...
 2n       1         n      if first n and last n knots are equal: Bézier case
 -------- --------- ------ -------
-	"""
-	def __init__(self, knots, degree=0):
-		self.knots = np.array(knots, float)
-		self.degree = degree
+    """
+    def __init__(self, knots, degree=0):
+        self.knots = np.array(knots, float)
+        self.degree = degree
 
-	def __repr__(self):
-		return "<{} polynomials of degree {}>".format(self.nb_curves, self.degree)
+    def __repr__(self):
+        return "<{} polynomials of degree {}>".format(self.nb_curves, self.degree)
 
-	@property
-	def nb_curves(self):
-		return  len(self.knots) - 2*self.degree + 1
+    @property
+    def nb_curves(self):
+        return  len(self.knots) - 2*self.degree + 1
 
-	def __getitem__(self, index):
-		return self.knots[index]
+    def __getitem__(self, index):
+        return self.knots[index]
 
-	ktol = 1e-13
+    ktol = 1e-13
 
-	def left_knot(self, t):
-		"""
-		Find out between which node a time t is.
-		"""
-		diff = self.knots[self.degree-1:-self.degree+1] - t
-		isrightof = diff > self.ktol
-		if np.all(isrightof):
-			raise ValueError("Time too small")
-		if np.all(~isrightof):
-			raise ValueError("Time too big")
-		left = np.argmax(isrightof) - 1 # argmax gives the right knot...
-		return left + self.degree-1
+    def left_knot(self, t):
+        """
+        Find out between which node a time t is.
+        """
+        diff = self.knots[self.degree-1:-self.degree+1] - t
+        isrightof = diff > self.ktol
+        if np.all(isrightof):
+            raise ValueError("Time too small")
+        if np.all(~isrightof):
+            raise ValueError("Time too big")
+        left = np.argmax(isrightof) - 1 # argmax gives the right knot...
+        return left + self.degree-1
 
-	def abscissae(self):
-		"""
-		Return the Greville abscissae.
-		"""
-		if self.degree == 0:
-			k = np.hstack([-1, self.knots])
-			return k
-		kernel = np.ones(self.degree)/self.degree
-		res = np.convolve(kernel, self.knots, 'valid')
-		return res
+    def abscissae(self):
+        """
+        Return the Greville abscissae.
+        """
+        if self.degree == 0:
+            k = np.hstack([-1, self.knots])
+            return k
+        kernel = np.ones(self.degree)/self.degree
+        res = np.convolve(kernel, self.knots, 'valid')
+        return res
 
-	def knot_range(self):
-		"""
+    def knot_range(self):
+        """
 The range of knots from which to generate the points.
-		"""
-		if self.degree == 0:
-			return []
-		return range(self.degree - 1, self.degree - 1 + self.nb_curves)
+        """
+        if self.degree == 0:
+            return []
+        return range(self.degree - 1, self.degree - 1 + self.nb_curves)
 
-	def intervals(self, knot_range=None):
-		"""
-		Compute the intervals from knot numbers `knot_range` till the next ones.
-		"""
-		if knot_range is None:
-			knot_range = self.knot_range()
-		for k in knot_range:
-			width = self.knots[k+1]-self.knots[k]
-			left, right = self.knots[k], self.knots[k+1]
-			yield (k, left, right)
+    def intervals(self, knot_range=None):
+        """
+        Compute the intervals from knot numbers `knot_range` till the next ones.
+        """
+        if knot_range is None:
+            knot_range = self.knot_range()
+        for k in knot_range:
+            width = self.knots[k+1]-self.knots[k]
+            left, right = self.knots[k], self.knots[k+1]
+            yield (k, left, right)
 
-	def get_basis(self, k=None):
-		if k is None:
-			k = self.degree
-		abscissae = self.abscissae()
-		pts = np.zeros([len(abscissae), 2])
-		pts[:,0] = self.abscissae()
-		pts[k,1] = 1.
-		return BSpline(self.knots, pts)
+    def get_basis(self, k=None):
+        if k is None:
+            k = self.degree
+        abscissae = self.abscissae()
+        pts = np.zeros([len(abscissae), 2])
+        pts[:,0] = self.abscissae()
+        pts[k,1] = 1.
+        return BSpline(self.knots, pts)
 
 
 def geodesic(P1, P2, theta):
-	"""
-	The geodesic between two points.
-	"""
-	return (1-theta)*P1 + theta*P2
+    """
+    The geodesic between two points.
+    """
+    return (1-theta)*P1 + theta*P2
 
 class BSpline(object):
-	def __init__(self, knots, control_points):
-		degree = len(knots) - len(control_points) + 1
-		self.knots = Knots(knots, degree)
-		self.control_points = np.array(control_points, float)
+    def __init__(self, knots, control_points):
+        degree = len(knots) - len(control_points) + 1
+        self.knots = Knots(knots, degree)
+        self.control_points = np.array(control_points, float)
 
-	def __call__(self, t, lknot=None):
-		t = np.array(t)
-		if lknot is None:
-			lknot = self.knots.left_knot(t.flatten()[0])
-		time_shape = (1,)*len(np.shape(t)) # time shape to add for broadcasting
+    def __call__(self, t, lknot=None):
+        t = np.array(t)
+        if lknot is None:
+            lknot = self.knots.left_knot(t.flatten()[0])
+        time_shape = (1,)*len(np.shape(t)) # time shape to add for broadcasting
 
-		pts = self.control_points[lknot-self.knots.degree + 1:lknot+2]
-		kns = self.knots[lknot - self.knots.degree + 1:lknot + self.knots.degree + 1]
-		kns.shape = kns.shape + time_shape # (K, 1)
-		if len(pts) != self.knots.degree + 1: # equivalent condition: len(kns) != 2*self.knots.degree
-			raise ValueError("Wrong knot index.")
+        pts = self.control_points[lknot-self.knots.degree + 1:lknot+2]
+        kns = self.knots[lknot - self.knots.degree + 1:lknot + self.knots.degree + 1]
+        kns.shape = kns.shape + time_shape # (K, 1)
+        if len(pts) != self.knots.degree + 1: # equivalent condition: len(kns) != 2*self.knots.degree
+            raise ValueError("Wrong knot index.")
 
-		# we put the time on the last index
-		pts.shape = pts.shape + time_shape # (K, D, 1)
+        # we put the time on the last index
+        pts.shape = pts.shape + time_shape # (K, D, 1)
 
-		for n in reversed(1+np.arange(self.knots.degree)):
-			diffs = kns[n:] - kns[:-n] # (K,1)
-			# trick to handle cases of equal knots:
-			diffs[diffs==0.] = np.finfo(kns.dtype).eps
-			rcoeff = (t - kns[:-n])/diffs # (K,T)
-			pts = geodesic(pts[:-1], pts[1:], rcoeff[:,np.newaxis,...]) # (K, D, 1), (K, 1, T)
-			kns = kns[1:-1]
-		result = pts[0] # (D, T)
-		return result.transpose() # (T, D)
-
-
-	plotres = 200
-	knot_style = {
-			'marker':'o',
-			'linestyle':'none',
-			'markerfacecolor':'white',
-			'markersize':5,
-			'markeredgecolor':'black',
-			}
-	control_style={
- 			'marker':'o',
-			'linestyle':':',
-			'color':'black',
-			'markersize':10,
-			'markerfacecolor':'white',
-			'markeredgecolor':'red'
-			}
-
-	def plot_knots(self):
-		ints = list(self.knots.intervals())
-		pts = [self(l,k) for k,l,r in ints]
-		pts.append(self(ints[-1][2], ints[-1][0])) # add last knot as well
-		apts = np.array(pts)
-		plt.plot(apts[:,0],apts[:,1], **self.knot_style)
+        for n in reversed(1+np.arange(self.knots.degree)):
+            diffs = kns[n:] - kns[:-n] # (K,1)
+            # trick to handle cases of equal knots:
+            diffs[diffs==0.] = np.finfo(kns.dtype).eps
+            rcoeff = (t - kns[:-n])/diffs # (K,T)
+            pts = geodesic(pts[:-1], pts[1:], rcoeff[:,np.newaxis,...]) # (K, D, 1), (K, 1, T)
+            kns = kns[1:-1]
+        result = pts[0] # (D, T)
+        return result.transpose() # (T, D)
 
 
-	def plot_control_points(self):
-		"""
-		Plot the control points.
-		"""
-		plt.plot(self.control_points[:,0],self.control_points[:,1], **self.control_style)
+    plotres = 200
+    knot_style = {
+            'marker':'o',
+            'linestyle':'none',
+            'markerfacecolor':'white',
+            'markersize':5,
+            'markeredgecolor':'black',
+            }
+    control_style={
+            'marker':'o',
+            'linestyle':':',
+            'color':'black',
+            'markersize':10,
+            'markerfacecolor':'white',
+            'markeredgecolor':'red'
+            }
 
-	def plot(self, knot=None, with_knots=False, margin=0.):
-		"""
-		Plot the curve.
-		"""
-		self.plot_control_points()
-		for k, left, right in self.knots.intervals(knot):
-			ts = np.linspace(left, right, self.plotres)
-			val = self(ts, lknot=k)
-			plt.plot(val[:,0],val[:,1], label="{:1.0f} - {:1.0f}".format(self.knots[k], self.knots[k+1]), lw=2)
-		if with_knots:
-			self.plot_knots()
+    def plot_knots(self):
+        ints = list(self.knots.intervals())
+        pts = [self(l,k) for k,l,r in ints]
+        pts.append(self(ints[-1][2], ints[-1][0])) # add last knot as well
+        apts = np.array(pts)
+        plt.plot(apts[:,0],apts[:,1], **self.knot_style)
+
+
+    def plot_control_points(self):
+        """
+        Plot the control points.
+        """
+        plt.plot(self.control_points[:,0],self.control_points[:,1], **self.control_style)
+
+    def plot(self, knot=None, with_knots=False, margin=0.):
+        """
+        Plot the curve.
+        """
+        self.plot_control_points()
+        for k, left, right in self.knots.intervals(knot):
+            ts = np.linspace(left, right, self.plotres)
+            val = self(ts, lknot=k)
+            plt.plot(val[:,0],val[:,1], label="{:1.0f} - {:1.0f}".format(self.knots[k], self.knots[k+1]), lw=2)
+        if with_knots:
+            self.plot_knots()
 
 
 
 class Bezier(BSpline):
-	"""
+    """
 Special case of a BSpline. For n+1 points, the knot list is [0]*n+[1]*n.
-	"""
-	def __init__(self, control_points):
-		degree = len(control_points) - 1
-		knots = np.zeros(2*degree)
-		knots[degree:] = 1
-		super(Bezier,self).__init__(knots, control_points)
+    """
+    def __init__(self, control_points):
+        degree = len(control_points) - 1
+        knots = np.zeros(2*degree)
+        knots[degree:] = 1
+        super(Bezier,self).__init__(knots, control_points)
 
 def get_basis_knots(x):
-	"""
-	Knots corresponding to the points in the array x.
-	The corresponding basis is obtained by get_basis_knots(x).get_basis()
-	"""
-	x = np.array(x)
-	degree = len(x) - 2
-	knots = np.hstack([(degree-1)*[x[0]], x, (degree-1)*[x[-1]]])
-	return Knots(knots, degree)
+    """
+    Knots corresponding to the points in the array x.
+    The corresponding basis is obtained by get_basis_knots(x).get_basis()
+    """
+    x = np.array(x)
+    degree = len(x) - 2
+    knots = np.hstack([(degree-1)*[x[0]], x, (degree-1)*[x[-1]]])
+    return Knots(knots, degree)
 
 
--- test_spline.py
+++ test_spline.py
@@ -8,167 +8,167 @@ import unittest
 from spline import *
 
 def get_canonical_knots(n):
-	knots = np.arange(3*n) - (3*n-1)/2
-	knots[:n-1] = knots[n-1]
-	knots[-(n-1):] = knots[-n]
-	return Knots(knots, degree=n)
+    knots = np.arange(3*n) - (3*n-1)/2
+    knots[:n-1] = knots[n-1]
+    knots[-(n-1):] = knots[-n]
+    return Knots(knots, degree=n)
 
 
 def get_basis(n):
-	nb_pts = 2*n+1
-	knots = np.arange(3*n) - (3*n-1)/2
-	control_points = np.vstack([np.arange(nb_pts),np.zeros(nb_pts)]).T
-	control_points[n,1] = 1.
+    nb_pts = 2*n+1
+    knots = np.arange(3*n) - (3*n-1)/2
+    control_points = np.vstack([np.arange(nb_pts),np.zeros(nb_pts)]).T
+    control_points[n,1] = 1.
 
-	spline = BSpline(knots, control_points)
-	return spline
+    spline = BSpline(knots, control_points)
+    return spline
 
 class TestBasis(unittest.TestCase):
-	def test_nonuniform(self):
-		a,b,c = 0., 2.5, 8
-		ck = get_basis_knots([a,b,c]).get_basis()
-		npt.assert_allclose(ck(a, lknot=0)[1], 0)
-		npt.assert_allclose(ck(b, lknot=1)[1], 1.)
-		npt.assert_allclose(ck(c, lknot=1)[1], 0.)
-		
-	def test_constant_abscissae(self):
-		k = get_basis_knots(np.arange(2))
-		k.abscissae()
+    def test_nonuniform(self):
+        a,b,c = 0., 2.5, 8
+        ck = get_basis_knots([a,b,c]).get_basis()
+        npt.assert_allclose(ck(a, lknot=0)[1], 0)
+        npt.assert_allclose(ck(b, lknot=1)[1], 1.)
+        npt.assert_allclose(ck(c, lknot=1)[1], 0.)
+        
+    def test_constant_abscissae(self):
+        k = get_basis_knots(np.arange(2))
+        k.abscissae()
 
 
-	## def test_canonical(self):
-	## 	ck = get_canonical_knots(5)
-	## 	cb = ck.get_basis()
-	## 	k = get_basis_knots(np.arange(5) - 2)
-	## 	kb = k.get_basis()
-	## 	b = get_basis(5)
-	## 	npt.assert_allclose(cb(0.), b(0.))
+    ## def test_canonical(self):
+    ##      ck = get_canonical_knots(5)
+    ##      cb = ck.get_basis()
+    ##      k = get_basis_knots(np.arange(5) - 2)
+    ##      kb = k.get_basis()
+    ##      b = get_basis(5)
+    ##      npt.assert_allclose(cb(0.), b(0.))
 
 
 class TestBezier(unittest.TestCase):
-	def setUp(self):
-		controls = [[1.,1],[0,-1],[-1,1]]
-		self.b = Bezier(controls)
+    def setUp(self):
+        controls = [[1.,1],[0,-1],[-1,1]]
+        self.b = Bezier(controls)
 
-	def test_quad(self):
-		u"""
-	Check that Bézier with three points generates the parabola y=x**2.
-		"""
-		b = self.b
-		self.assertEqual(b.knots.nb_curves,1)
-		ts = np.linspace(0.,1., 200)
-		all_pts = b(ts)
-		npt.assert_array_almost_equal(all_pts[:,0]**2, all_pts[:,1])
-		npt.assert_allclose(b(.5), 0.)
+    def test_quad(self):
+        u"""
+    Check that Bézier with three points generates the parabola y=x**2.
+        """
+        b = self.b
+        self.assertEqual(b.knots.nb_curves,1)
+        ts = np.linspace(0.,1., 200)
+        all_pts = b(ts)
+        npt.assert_array_almost_equal(all_pts[:,0]**2, all_pts[:,1])
+        npt.assert_allclose(b(.5), 0.)
 
-	def test_intervals(self):
-		intervals = list(self.b.knots.intervals())
-		self.assertEqual(len(intervals), self.b.knots.nb_curves)
+    def test_intervals(self):
+        intervals = list(self.b.knots.intervals())
+        self.assertEqual(len(intervals), self.b.knots.nb_curves)
 
-	def test_left_knot(self):
-		self.assertEqual(self.b.knots.left_knot(.2), 1)
-		self.assertEqual(self.b.knots.left_knot(.8), 1)
+    def test_left_knot(self):
+        self.assertEqual(self.b.knots.left_knot(.2), 1)
+        self.assertEqual(self.b.knots.left_knot(.8), 1)
 
-	def test_plot(self):
-		self.b.plot()
+    def test_plot(self):
+        self.b.plot()
 
 
 class Test_DoubleQuad(unittest.TestCase):
-	def setUp(self):
-		controls = [[-1,1],[0,-1],[2.,3],[3,1]]
-		knots = [0,0,.5,1,1]
-		self.spline = BSpline(knots, controls)
+    def setUp(self):
+        controls = [[-1,1],[0,-1],[2.,3],[3,1]]
+        knots = [0,0,.5,1,1]
+        self.spline = BSpline(knots, controls)
 
-	def test_info(self):
-		self.assertEqual(self.spline.knots.degree, 2)
-		self.assertEqual(self.spline.knots.nb_curves,2)
-		self.assertEqual(len(self.spline.knots.knot_range()), self.spline.knots.nb_curves)
+    def test_info(self):
+        self.assertEqual(self.spline.knots.degree, 2)
+        self.assertEqual(self.spline.knots.nb_curves,2)
+        self.assertEqual(len(self.spline.knots.knot_range()), self.spline.knots.nb_curves)
 
-	def test_generate(self):
-		intervals = list(self.spline.knots.intervals())
-		self.assertEqual(len(intervals), self.spline.knots.nb_curves)
-		a0,a1 = [self.spline(np.linspace(l,r,200)) for (k,l,r) in intervals]
-		npt.assert_array_almost_equal(a0[:,0]**2, a0[:,1])
-		npt.assert_array_almost_equal(-(a1[:,0]-2)**2, a1[:,1]-2)
+    def test_generate(self):
+        intervals = list(self.spline.knots.intervals())
+        self.assertEqual(len(intervals), self.spline.knots.nb_curves)
+        a0,a1 = [self.spline(np.linspace(l,r,200)) for (k,l,r) in intervals]
+        npt.assert_array_almost_equal(a0[:,0]**2, a0[:,1])
+        npt.assert_array_almost_equal(-(a1[:,0]-2)**2, a1[:,1]-2)
 
 class Test_BSpline(unittest.TestCase):
-	def setUp(self):
-		ex2 = {
-		'control_points': np.array([[1.,2], [2,3], [2,5], [1,6], [1,9]]),
-		'knots': np.array([1.,2.,3.,4.,5.,6.,7.])
-		}
-		self.b = BSpline(**ex2)
+    def setUp(self):
+        ex2 = {
+        'control_points': np.array([[1.,2], [2,3], [2,5], [1,6], [1,9]]),
+        'knots': np.array([1.,2.,3.,4.,5.,6.,7.])
+        }
+        self.b = BSpline(**ex2)
 
-	def test_left_knot(self):
-		self.assertEqual(self.b.knots.left_knot(3.8), 2)
-		self.assertEqual(self.b.knots.left_knot(3.2), 2)
-		self.assertEqual(self.b.knots.left_knot(4.8), 3)
-		self.assertEqual(self.b.knots.left_knot(4.0), 3)
-		self.assertEqual(self.b.knots.left_knot(4.0-1e-14), 3)
-		with self.assertRaises(ValueError):
-			self.b.knots.left_knot(2.5)
-		with self.assertRaises(ValueError):
-			self.b.knots.left_knot(5.5)
+    def test_left_knot(self):
+        self.assertEqual(self.b.knots.left_knot(3.8), 2)
+        self.assertEqual(self.b.knots.left_knot(3.2), 2)
+        self.assertEqual(self.b.knots.left_knot(4.8), 3)
+        self.assertEqual(self.b.knots.left_knot(4.0), 3)
+        self.assertEqual(self.b.knots.left_knot(4.0-1e-14), 3)
+        with self.assertRaises(ValueError):
+            self.b.knots.left_knot(2.5)
+        with self.assertRaises(ValueError):
+            self.b.knots.left_knot(5.5)
 
-	def test_knot_range(self):
-		k = Knots(np.arange(10))
-		self.assertEqual(len(k.knot_range()), 0)
+    def test_knot_range(self):
+        k = Knots(np.arange(10))
+        self.assertEqual(len(k.knot_range()), 0)
 
-	def test_wrong_knot(self):
-		with self.assertRaises(ValueError):
-			self.b(3.5, lknot=1)
-		with self.assertRaises(ValueError):
-			self.b(3.5, lknot=4)
+    def test_wrong_knot(self):
+        with self.assertRaises(ValueError):
+            self.b(3.5, lknot=1)
+        with self.assertRaises(ValueError):
+            self.b(3.5, lknot=4)
 
-	def test_abscissae(self):
-		pts = np.random.random_sample([7,2])
-		knots = [0,0,0,2,3,4,5,5,5]
-		b = BSpline(knots, pts)
-		computed = b.knots.abscissae()
-		expected = np.array([0, 2/3, 5/3, 3, 4, 14/3, 5]) # values from Sederberg §6.14
-		npt.assert_allclose(computed, expected)
+    def test_abscissae(self):
+        pts = np.random.random_sample([7,2])
+        knots = [0,0,0,2,3,4,5,5,5]
+        b = BSpline(knots, pts)
+        computed = b.knots.abscissae()
+        expected = np.array([0, 2/3, 5/3, 3, 4, 14/3, 5]) # values from Sederberg §6.14
+        npt.assert_allclose(computed, expected)
 
-	def test_plot(self):
-		self.b.plot(with_knots=True)
+    def test_plot(self):
+        self.b.plot(with_knots=True)
 
 
 class Test_BSpline3(unittest.TestCase):
-	def setUp(self):
-		ex2 = {
-		'control_points': np.array([[1.,2,0], [2,3,1], [2,5,3], [1,6,3], [1,9,2]]),
-		'knots': np.array([1.,2.,3.,4.,5.,6.,7.])
-		}
-		self.b = BSpline(**ex2)
+    def setUp(self):
+        ex2 = {
+        'control_points': np.array([[1.,2,0], [2,3,1], [2,5,3], [1,6,3], [1,9,2]]),
+        'knots': np.array([1.,2.,3.,4.,5.,6.,7.])
+        }
+        self.b = BSpline(**ex2)
 
-	def test_call(self):
-		self.b(3.5)
+    def test_call(self):
+        self.b(3.5)
 
-	def test_scalar_shape(self):
-		self.assertEqual(np.shape(self.b(3.5)), (3,))
+    def test_scalar_shape(self):
+        self.assertEqual(np.shape(self.b(3.5)), (3,))
 
 class TestKnots(unittest.TestCase):
-	def test_basis(self):
-		"""
-		Check that the basis functions sum up to one.
-		"""
-		w = [ 0, 0, 0, 1/3, 2/3, 1, 1, 1]
-		wk = Knots(w, degree=3)
-		basis = [wk.get_basis(i) for i in range(6)]
-		for k,l,r in wk.intervals():
-			ts = np.linspace(l,r,30)
-			vals = np.array([b(ts, lknot=k) for b in basis])
-			npt.assert_allclose(np.sum(vals[:,:,1], axis=0), 1.)
+    def test_basis(self):
+        """
+        Check that the basis functions sum up to one.
+        """
+        w = [ 0, 0, 0, 1/3, 2/3, 1, 1, 1]
+        wk = Knots(w, degree=3)
+        basis = [wk.get_basis(i) for i in range(6)]
+        for k,l,r in wk.intervals():
+            ts = np.linspace(l,r,30)
+            vals = np.array([b(ts, lknot=k) for b in basis])
+            npt.assert_allclose(np.sum(vals[:,:,1], axis=0), 1.)
 
 import os
 
 class TestDemo(unittest.TestCase):
-	@unittest.skip("Testing the demo notebook")
-	def test_demo(self):
-		import nbformat
-		from nbconvert.preprocessors.execute import ExecutePreprocessor
-		here = os.path.dirname(__file__)
-		demo = os.path.join(here,'Demo.ipynb')
-		nb = nbformat.read(demo, as_version=4)
-		pp = ExecutePreprocessor()
-		pp.allow_errors = False
-		pp.preprocess(nb, resources={})
+    @unittest.skip("Testing the demo notebook")
+    def test_demo(self):
+        import nbformat
+        from nbconvert.preprocessors.execute import ExecutePreprocessor
+        here = os.path.dirname(__file__)
+        demo = os.path.join(here,'Demo.ipynb')
+        nb = nbformat.read(demo, as_version=4)
+        pp = ExecutePreprocessor()
+        pp.allow_errors = False
+        pp.preprocess(nb, resources={})
