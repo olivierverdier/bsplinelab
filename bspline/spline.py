6fd6e75b5838568a12bb2f41a41662a06a2d2cb2
Olivier Verdier <olivier.verdier@gmail.com>
2016-01-19 21:52:33 +0100

    small refactoring the BSpline.__call__ function was getting too big

--- bspline/spline.py
+++ bspline/spline.py
@@ -14,24 +14,11 @@ class BSpline(object):
         self.control_points = np.array(control_points) 
         self.geometry = geometry
 
+    def data_dim(self):
+        return np.ndim(self.control_points[0])
 
-    def __call__(self, t, lknot=None):
-        t = np.array(t)
-        if lknot is None:
-            lknot = self.knots.left_knot(t.flatten()[0])
-        time_shape = (1,)*len(np.shape(t)) # time shape to add for broadcasting
-
-        pts = self.control_points[lknot-self.knots.degree + 1:lknot+2]
-        if len(pts) != self.knots.degree + 1: # equivalent condition: len(kns) != 2*self.knots.degree
-            raise ValueError("Wrong knot index.")
-
-        data_dim = np.ndim(pts[0]) # data dim to use for broadcasting
-
-        kns = self.knots[lknot - self.knots.degree + 1:lknot + self.knots.degree + 1]
-        kns.shape = kns.shape + time_shape # (K, 1)
-
-        # we put the time on the last index
-        pts.shape = pts.shape + time_shape # (K, D, 1)
+    def iterate(self, kns, pts, t):
+        data_dim = self.data_dim() # data dim to use for broadcasting
 
         # reshape the coefficients using data dimension and possible time shape
         # for vectorial data, this amounts to the slice (:, np.newaxis,...)
@@ -44,10 +31,29 @@ class BSpline(object):
             rcoeff = (t - kns[:-n])/diffs # (K,T)
             pts = self.geometry(pts[:-1], pts[1:], rcoeff[rcoeff_slice]) # (K, D, 1), (K, 1, T)
             kns = kns[1:-1]
+
         result = pts[0] # (D, T)
         # put time first by permuting the indices; in the vector case, this is a standard permutation
         permutation = len(np.shape(t))*(data_dim,) + tuple(range(data_dim))
         return result.transpose(permutation) # (T, D)
+
+    def __call__(self, t, lknot=None):
+        t = np.array(t)
+        if lknot is None:
+            lknot = self.knots.left_knot(t.flatten()[0])
+        time_shape = (1,)*len(np.shape(t)) # time shape to add for broadcasting
+
+        pts = self.control_points[lknot-self.knots.degree + 1:lknot+2]
+        if len(pts) != self.knots.degree + 1: # equivalent condition: len(kns) != 2*self.knots.degree
+            raise ValueError("Wrong knot index.")
+
+        kns = self.knots[lknot - self.knots.degree + 1:lknot + self.knots.degree + 1]
+        kns.shape = kns.shape + time_shape # (K, 1)
+
+        # we put the time on the last index
+        pts.shape = pts.shape + time_shape # (K, D, 1)
+
+        return self.iterate(kns, pts, t) # (D, T)
 
 
 
