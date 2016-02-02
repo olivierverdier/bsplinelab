465bf766233e0d7dbb406604686ecd362c34545a
unknown <geirb@MATH-PC352.net.chalmers.se>
2016-02-03 11:10:53 +1300

    Refactoring and comments of c2spline

--- bspline/c2spline.py
+++ bspline/c2spline.py
@@ -9,26 +9,32 @@ from .geometry import Geometry
 from .spline import BSpline
 
 def c2spline(interpolation_points, initial_control_points, geometry = Geometry()):
-    
+    # initialize the array for control points of the final spline.
+    control_points=np.zeros([interpolation_points.shape[0]*3-2, interpolation_points.shape[1]]) 
+    # Set up for main loop
     P = interpolation_points[0]
     prev_control_points = initial_control_points[0:2]
-    k=0
-    control_points=np.zeros([interpolation_points.shape[0]*3-2, interpolation_points.shape[1]])
-    control_points[k] = P
-    control_points[k+1:k+3]=prev_control_points
-    w=geometry.log(prev_control_points[1], prev_control_points[0])
-    k=k+3
     new_control_points=np.zeros(prev_control_points.shape)
+    # Copy initial data to control_points
+    control_points[0] = P
+    control_points[1:3]=prev_control_points
+
+    k=3
+    
     for P in interpolation_points[1:-1]:
+        # Calculate the new control points
         new_control_points[0]=geometry.involution(P, prev_control_points[1])
+        w=geometry.log(prev_control_points[1], prev_control_points[0])
         u = -geometry.involution_derivative(P, prev_control_points[1], w)-2*geometry.log(new_control_points[0], P)
         new_control_points[1]=geometry.exp(new_control_points[0], u)
-        prev_control_points=new_control_points
+        
+        #copy data to control_points
         control_points[k] = P
-        control_points[k+1:k+3]=prev_control_points
-        w=geometry.log(prev_control_points[1], prev_control_points[0])
+        control_points[k+1:k+3]=new_control_points
         k=k+3
-    control_points[-1]=interpolation_points[-1]
+        prev_control_points=np.copy(new_control_points)
+    #insert final interpolation point at end
+    control_points[k]=interpolation_points[-1]
     
     ex = {
     'control_points': control_points,
