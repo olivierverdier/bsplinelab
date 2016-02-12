4552be8362e05743bfb60fe7203e19612d82236c
Klas Modin <klas.modin@gmail.com>
2016-02-12 15:16:58 +1300

    Added 3d plot on the sphere in the example demo file. This code should probably be implemented in bspline.plotting and used whenever geometry=Sphere_geometry().

--- implicitc2spline_demo.py
+++ implicitc2spline_demo.py
@@ -5,21 +5,12 @@ Created on Thu Jan 21 09:25:42 2016
 @author: geirb
 """
 from __future__ import division
-
-
-
-
 import numpy as np
-
-
 from bspline import geometry
-
 from bspline.c2spline import implicitc2spline
-
 import bspline.plotting as splt
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
-
-
 
 
 N=8
@@ -39,25 +30,41 @@ b= implicitc2spline(interpolation_points, boundary_velocities, geometry=geometry
 #print((b(t+1.5*h)-3*b(t+0.5*h)+3*b(t-0.5*h)-b(t-1.5*h))/(h*h).reshape(h.shape +(1,))) 
 #print("If C_2, these should approach zero.")
 
+# Plot using bspline.plotting (currently projected 2D plot)
+if True:
+	fig1=plt.figure(1)
+	splt.plot(b, with_control_points=False)
+	fig1.suptitle('Spline on S^2, stereographically projected onto R^2')
+	t=np.floor(N/2)
 
-fig1=plt.figure(1)
-splt.plot(b, with_control_points=False)
-fig1.suptitle('Spline on S^2, stereographically projected onto R^2')
+# Plot second derivative
+if True:
+	hh=1e-5
+	ts = np.linspace(0.,N-1,(N-1)*100)[1:-1]
+	ddb = np.zeros((ts.shape[0],3))
+	for (tt,ddbt) in zip(ts,ddb):
+	    ddbt[:] = (b(tt+hh)-2*b(tt)+b(tt-hh))/hh**2
+	fig2 = plt.figure(2)
+	plt.plot(ddb[:,0], ddb[:,1], linestyle='None', marker=',')
+	fig2.suptitle('Second derivative of spline')
 
-t=np.floor(N/2)
+# Plot on the embedded sphere
+if True:
+	fig3=plt.figure(3)
+	ax = fig3.add_subplot(111, projection='3d')
+	u = np.linspace(0, 2 * np.pi, 100)
+	v = np.linspace(0, np.pi, 100)
+	xx = 0.99*np.outer(np.cos(u), np.sin(v))
+	yy = 0.99*np.outer(np.sin(u), np.sin(v))
+	zz = 0.99*np.outer(np.ones(np.size(u)), np.cos(v))
+	ax.plot_wireframe(xx, yy, zz, rstride=4, cstride=4, color='0.8', linewidth=1)
+	ts = np.linspace(0.,N-1,1000)
+	bvals = np.zeros((ts.shape[0],3))
+	for (tt,bval) in zip(ts,bvals):
+		bval[:] = b(tt)
+	ax.plot(bvals[:,0],bvals[:,1],bvals[:,2],linewidth=2)
+	fig3.suptitle('Spline on embedded sphere')
 
-
-hh=1e-5
-ts = np.linspace(0.,N-1, (N-1)*100)
-ts= ts[1:-1]
-ddb=np.array([]).reshape(0,3)
-for tt in ts:
-    temp=np.array((b(tt+hh)-2*b(tt)+b(tt-hh))/(hh*hh))
-    temp=temp[np.newaxis, :]
-    ddb = np.append(ddb, temp, axis=0) # There should be a better way of doing this.
-fig2=plt.figure(2)
-plt.plot(ddb[:,0],ddb[:, 1],linestyle='None', marker=',')
-fig2.suptitle('Second derivative of spline')
 
 #fig3=plt.figure(3)
 #plt.plot(ddb[(ts>t-0.1)*(ts<t+0.1),0], ddb[(ts>t-0.1)*(ts<t+0.1),1],linestyle='None', marker=',')
