6e11c8bb12219e15aa23fb88f8015deec7161458
Geir Bogfjellmo <geirb@chalmers.se>
2016-11-24 13:13:40 +0100

    Grassmannian geometry added.

---
+++ implicitc2spline_Gr_demo.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 21 09:25:42 2016
+
+@author: geirb
+"""
+from __future__ import division
+import numpy as np
+from bspline import geometry
+from bspline.c2spline import implicitc2spline
+import matplotlib.pyplot as plt
+
+
+alpha = 0.01
+N=5
+n=10
+k=2
+P1 = np.vstack((np.eye(k), np.zeros((n-k,k))))
+interpolation_points= np.tile(P1, (N,1,1))
+
+np.random.seed(0)
+for i in range(1,N):
+    P2 = P1+1.0*np.random.randn(n,k)
+    interpolation_points[i] = np.linalg.qr(P2)[0]
+
+
+
+
+b= implicitc2spline(interpolation_points, geometry=geometry.Grassmannian(), Maxiter=1000, tol = 1e-14)
+
+
+
+if True:
+    fig3=plt.figure(3)
+    ax = fig3.add_subplot(111, projection='3d')
+    tl = np.linspace(0.,N-1,1000)
+    bvals =b(tl)
+    bb = np.einsum('...ij,...kj', bvals,bvals)
+    ax.plot(bb[:,0,0],bb[:,1,0],bb[:,2,0],linewidth=2)
+    ax.plot(bb[:,0,1],bb[:,1,1],bb[:,2,1],linewidth=2)
+    ax.plot(bb[:,0,2],bb[:,1,2],bb[:,2,2],linewidth=2 )
+    ax.plot(np.array([0.]),np.array([0.]),np.array([0.]), marker = '.', markersize=20)
+    
+    
+if True: # tests c2-continuity in interpolation point
+    def bmat(t): return np.einsum('...ij,...kj',b(t), b(t))
+    ttest = np.floor(0.5*N)
+    H=np.power(2.0, range(-2,-20,-1))
+    bmatdiv = (bmat(ttest+1.5*H)-3*bmat(ttest+0.5*H)+3*bmat(ttest-0.5*H)-bmat(ttest-1.5*H))/H[:, np.newaxis, np.newaxis]**2
+    dd=np.linalg.norm(bmatdiv, axis=(1,2))    
+    fig6=plt.figure(6)
+    plt.loglog(H, dd)
--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -63,3 +63,31 @@ class TestHyperboloid(unittest.TestCase):
             q = self.geom.exp(p,v)
             w = self.geom.log(p,q)
             npt.assert_allclose(v,w)        
+class TestGrassmannian(unittest.TestCase):
+    def setUp(self):
+        self.one_point = np.array([[1.,0], [0, 1], [0,0]])
+        self.init_vel= np.array([[0.,0], [0.,0], [np.pi/2, 0]])
+        self.geom= geometry.Grassmannian()
+        self.other_point = np.array([[0., 0], [0,1], [1,0]])
+        
+    def test_exp(self):
+        P = self.other_point
+        Q = self.geom.exp(self.one_point, self.init_vel)
+        npt.assert_allclose(P.dot(P.T), Q.dot(Q.T), atol=1e-15)
+    
+    def test_log(self):
+        npt.assert_allclose(self.init_vel, self.geom.log(self.one_point, self.other_point), atol=1e-15)
+        
+    def test_exp_log(self):
+        np.random.seed(1)
+        for i in range(2,50):
+            for j in range(1,min(i,40)):
+                p = np.random.randn(i,j)
+                p = np.linalg.qr(p)[0]
+                v = np.array([4])
+                while np.linalg.norm(v, 2)>=0.5* np.pi:
+                    v = np.random.randn(i,j)/(100*np.sqrt(i))
+                    v = v-p.dot(p.T).dot(v) # norm(v)> pi might cause problems.
+                q = self.geom.exp(p,v)
+                w = self.geom.log(p,q)
+                npt.assert_allclose(v,w, err_msg=str(i)+','+str(j), atol=1e-15)
