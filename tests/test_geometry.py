fa927ceea62f4d9dfc76e4667c09e55f14f27517
Olivier Verdier <olivier.verdier@gmail.com>
2016-12-06 11:48:17 +0100

    trailing white spaces

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -15,13 +15,13 @@ class TestSphere(unittest.TestCase):
         self.init_vel = 0.5*np.pi*np.array([0.,1,0])
         self.geom= geometry.Sphere_geometry()
         self.other_point = np.array([0.,1,0])
-        
+
     def test_exp(self):
         npt.assert_allclose(self.other_point, self.geom.exp(self.north_pole, self.init_vel), atol=1e-15)
-    
+
     def test_log(self):
         npt.assert_allclose(self.init_vel, self.geom.log(self.north_pole, self.other_point), atol=1e-15)
-        
+
     def test_exp_log(self):
         np.random.seed(1)
         for i in range(10):
@@ -33,21 +33,21 @@ class TestSphere(unittest.TestCase):
             q = self.geom.exp(p,v)
             w = self.geom.log(p,q)
             npt.assert_allclose(v,w)
-        
-        
+
+
 class TestHyperboloid(unittest.TestCase):
     def setUp(self):
         self.center = np.array([1.,0,0])
         self.init_vel = np.arccosh(2)*np.array([0.,1,0])
         self.geom= geometry.Hyperboloid_geometry()
         self.other_point = np.array([2,np.sqrt(3),0])
-        
+
     def test_exp(self):
         npt.assert_allclose(self.other_point, self.geom.exp(self.center, self.init_vel), atol=1e-15)
-    
+
     def test_log(self):
         npt.assert_allclose(self.init_vel, self.geom.log(self.center, self.other_point), atol=1e-15)
-        
+
     def test_exp_log(self):
         np.random.seed(1)
         for i in range(10):
@@ -55,25 +55,25 @@ class TestHyperboloid(unittest.TestCase):
             p = np.insert(pp, 0, np.sqrt(1+np.linalg.norm(pp)**2))
             vv=np.random.randn(3)
             v = np.insert(vv, 0, np.inner(pp,vv)/p[0])
-        
+
             q = self.geom.exp(p,v)
             w = self.geom.log(p,q)
-            npt.assert_allclose(v,w)        
+            npt.assert_allclose(v,w)
 class TestGrassmannian(unittest.TestCase):
     def setUp(self):
         self.one_point = np.array([[1.,0], [0, 1], [0,0]])
         self.init_vel= np.array([[0.,0], [0.,0], [np.pi/2, 0]])
         self.geom= geometry.Grassmannian()
         self.other_point = np.array([[0., 0], [0,1], [1,0]])
-        
+
     def test_exp(self):
         P = self.other_point
         Q = self.geom.exp(self.one_point, self.init_vel)
         npt.assert_allclose(P.dot(P.T), Q.dot(Q.T), atol=1e-15)
-    
+
     def test_log(self):
         npt.assert_allclose(self.init_vel, self.geom.log(self.one_point, self.other_point), atol=1e-15)
-        
+
     def test_exp_log(self):
         np.random.seed(1)
         for i in range(2,50):
