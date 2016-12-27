4909389b26a0f3c0b5e61853ea0515a3a80733e8
Olivier Verdier <olivier.verdier@gmail.com>
2016-12-27 20:36:22 +0100

    prepare class splitting

--- bspline/symmetric.py
+++ bspline/symmetric.py
@@ -22,31 +22,6 @@ class Interpolator():
         [boundary.initialize(self) for boundary in self.boundaries]
         self.postmortem = {}
 
-    def generate_controls(self, points, velocities):
-        """
-        Generate movements and control points.
-        """
-        for p, v in zip(points, velocities):
-            g = self.geometry.redexp(p, v)
-            control = self.geometry.action(g, p)
-            yield g, control
-
-    def iterate(self, velocities):
-        gRs, qRs = list(zip(*self.generate_controls(self.interpolation_points[:-1], velocities[:-1])))
-        gLs, qLs = list(zip(*self.generate_controls(self.interpolation_points[1:], -velocities[1:])))
-        delta = np.zeros_like(velocities[1:-1])
-        gen = zip(
-            self.interpolation_points[1:-1],
-            velocities[1:-1],
-            gLs[:-1],
-            qLs[1:],
-            gRs[1:],
-            qRs[:-1],
-        )
-        for i, (p, v, gL, qL, gR, qR) in enumerate(gen):
-            delta[i] = self.geometry.log(p, self.geometry.action(gL, qL)) - self.geometry.log(p, self.geometry.action(gR, qR)) - 2*v
-        return qRs, qLs, delta/4
-
     def compute_controls(self):
         """
         Main fixed point algorithm.
@@ -89,6 +64,31 @@ class Interpolator():
         return BSpline(control_points=spline_control_points,
                        knots=self.get_knots(),
                        geometry=self.geometry)
+
+    def generate_controls(self, points, velocities):
+        """
+        Generate movements and control points.
+        """
+        for p, v in zip(points, velocities):
+            g = self.geometry.redexp(p, v)
+            control = self.geometry.action(g, p)
+            yield g, control
+
+    def iterate(self, velocities):
+        gRs, qRs = list(zip(*self.generate_controls(self.interpolation_points[:-1], velocities[:-1])))
+        gLs, qLs = list(zip(*self.generate_controls(self.interpolation_points[1:], -velocities[1:])))
+        delta = np.zeros_like(velocities[1:-1])
+        gen = zip(
+            self.interpolation_points[1:-1],
+            velocities[1:-1],
+            gLs[:-1],
+            qLs[1:],
+            gRs[1:],
+            qRs[:-1],
+        )
+        for i, (p, v, gL, qL, gR, qR) in enumerate(gen):
+            delta[i] = self.geometry.log(p, self.geometry.action(gL, qL)) - self.geometry.log(p, self.geometry.action(gR, qR)) - 2*v
+        return qRs, qLs, delta/4
 
 
 class Riemann(Interpolator):
